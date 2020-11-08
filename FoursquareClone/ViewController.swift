180f09a4431700aac134a76a148a54648c512a37
Ali Ertekin Elkoca <30657493+ertekinelkoca@users.noreply.github.com>
2020-11-08 22:23:06 +0300

    initial commit /  pod file created , parse fetched

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by mac on 8.11.2020.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
