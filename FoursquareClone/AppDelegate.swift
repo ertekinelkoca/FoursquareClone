0ed181fc57250713969125e92e33ac220eebeb98
Ali Ertekin Elkoca <30657493+ertekinelkoca@users.noreply.github.com>
2020-11-09 12:58:08 +0300

    signIn process done

--- FoursquareClone/AppDelegate.swift
+++ FoursquareClone/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         Parse.initialize(with: configuration)
-        
+
         return true
     }
 
--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -9,14 +9,58 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-         
     }
-
-
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        if usernameText.text != "" && passwordText.text != ""{
+            
+            PFUser.logInWithUsername(inBackground: usernameText.text!, password: passwordText.text!) { (user, error) in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                }
+                else{
+                    
+                    //Segue
+                            
+                }
+            }
+        }
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        if usernameText.text != "" && passwordText.text != ""{
+            
+            let user = PFUser()
+            user.username = usernameText.text!
+            user.password = passwordText.text!
+            
+            user.signUpInBackground { (success, error) in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "error!!!")
+                }
+                else{
+                    print("user created")
+                }
+            }
+            
+        }
+    }
+    
+    func makeAlert(titleInput : String , messageInput : String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
