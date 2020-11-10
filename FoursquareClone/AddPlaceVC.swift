8b9a2123323868083825e09317b1ef4675547807
Ali Ertekin Elkoca <30657493+ertekinelkoca@users.noreply.github.com>
2020-11-11 00:46:30 +0300

    image picker added to add places view controller, detailsVC created

--- FoursquareClone/AddPlaceVC.swift
+++ FoursquareClone/AddPlaceVC.swift
@@ -7,19 +7,33 @@
 
 import UIKit
 
-class AddPlaceVC: UIViewController {
+class AddPlaceVC: UIViewController , UINavigationControllerDelegate , UIImagePickerControllerDelegate{
 
     
     @IBOutlet weak var placeNameText: UITextField!
     @IBOutlet weak var placeTypeText: UITextField!
     @IBOutlet weak var placeAtmosphereText: UITextField!
-    
     @IBOutlet weak var placeImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        placeImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        placeImageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func chooseImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        placeImageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func nextButtonClicked(_ sender: Any) {
