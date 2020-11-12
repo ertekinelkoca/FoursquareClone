9fa2b30f7b9967487a45afcc3f106a6fa78d2325
Ali Ertekin Elkoca <30657493+ertekinelkoca@users.noreply.github.com>
2020-11-13 01:33:35 +0300

    Navigation Added

--- FoursquareClone/DetailsVC.swift
+++ FoursquareClone/DetailsVC.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 import Parse
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController , MKMapViewDelegate {
 
     @IBOutlet weak var detailsImageView: UIImageView!
     @IBOutlet weak var detailsNameLabel: UILabel!
@@ -25,6 +25,8 @@ class DetailsVC: UIViewController {
         super.viewDidLoad()
         //Parse
         getDataFromParse()
+        
+        detailsMapView.delegate = self
     }
     
     func getDataFromParse(){
@@ -33,40 +35,34 @@ class DetailsVC: UIViewController {
         query.findObjectsInBackground { (objects, error) in
             if error != nil {
                 
-            }else {
+            }
+            
+            else {
                 if objects != nil {
                     if objects!.count > 0 {
                         let chosenPlaceObject = objects![0]
-                        if let placeName = chosenPlaceObject.object(forKey: "name") as? String {
-                            
-                            self.detailsNameLabel.text = placeName
-                            
-                            }
-                        if let placeType = chosenPlaceObject.object(forKey: "type") as? String {
-                            
-                            self.detailsTypeLabel.text = placeType
-                            
-                            }
-                        if let placeAtmosphere = chosenPlaceObject.object(forKey: "atmosphere") as? String {
-                            
-                            self.detailsAtmosphereLabel.text = placeAtmosphere
-                            
-                        }
                         
+                        
+                        //The section working with object
+                        if let placeName = chosenPlaceObject.object(forKey: "name") as? String {
+                            self.detailsNameLabel.text = placeName
+                        }
+                        if let placeType = chosenPlaceObject.object(forKey: "type") as? String {
+                            self.detailsTypeLabel.text = placeType
+                        }
+                        if let placeAtmosphere = chosenPlaceObject.object(forKey: "atmosphere") as? String {
+                            self.detailsAtmosphereLabel.text = placeAtmosphere
+                        }
                         if let placeLatitude  = chosenPlaceObject.object(forKey: "latitude") as? String {
-                            
                             if let placeLatitudeDouble = Double(placeLatitude) {
                                 self.chosenLatitude = placeLatitudeDouble
                             }
                         }
-                        
                         if let placeLongitude = chosenPlaceObject.object(forKey: "longitude") as? String {
-                            
                             if let placeLongitudeDouble = Double(placeLongitude) {
                                 self.chosenLongitude = placeLongitudeDouble
                             }
                         }
-                        
                         if let imageData = chosenPlaceObject.object(forKey: "image") as? PFFileObject {
                             imageData.getDataInBackground { (data, error) in
                                 if error == nil {
@@ -76,11 +72,78 @@ class DetailsVC: UIViewController {
                                 }
                             }
                         }
+                        
+                        // Transferring object datas to the map
+                        
+                        let location = CLLocationCoordinate2D(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
+                        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                        let region = MKCoordinateRegion(center: location, span: span)
+                        self.detailsMapView.setRegion(region, animated: true)
+                        
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = location
+                        annotation.title = self.detailsNameLabel.text
+                        annotation.subtitle = self.detailsTypeLabel.text
+                        self.detailsMapView.addAnnotation(annotation)
+                        
                     }
                 }
             }
         }
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            //to show  visual section to put button on pinview
+            pinView?.canShowCallout = true
+            //to İ button
+            let button = UIButton(type: .detailDisclosure)
+            
+            pinView?.rightCalloutAccessoryView = button
+            
+        } else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    //what will happen then the button clicked
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if self.chosenLongitude != 0.0 && self.chosenLatitude != 0.0{
+            
+            
+            //place mark is created for to open navigation
+            let requestLocation = CLLocation(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
+            CLGeocoder().reverseGeocodeLocation(requestLocation) { (placeMarks, error) in
+                if let placemark = placeMarks{
+                    if placemark.count > 0 {
+                        let mkPlaceMark = MKPlacemark(placemark: placemark[0])
+                        //To open navigation
+                        let mapItem = MKMapItem(placemark: mkPlaceMark)
+                        mapItem.name = self.detailsNameLabel.text
+                        
+                        let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+                        
+                        //to start navigation
+                        mapItem.openInMaps(launchOptions: launchOptions)
+                        
+                    }
+            }
+            
+        }
+    }
+    
 }
 
-
+}
