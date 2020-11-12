812eb9ff50a8e7045631d6cd9e55df5bfd65aa3b
Ali Ertekin Elkoca <30657493+ertekinelkoca@users.noreply.github.com>
2020-11-12 20:02:53 +0300

    detailsVC Done

--- FoursquareClone/DetailsVC.swift
+++ FoursquareClone/DetailsVC.swift
@@ -18,22 +18,69 @@ class DetailsVC: UIViewController {
     @IBOutlet weak var detailsMapView: MKMapView!
     
     var chosenPlaceId = ""
+    var chosenLatitude = Double()
+    var chosenLongitude = Double()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(chosenPlaceId)
-        
+        //Parse
+        getDataFromParse()
+    }
+    
+    func getDataFromParse(){
         let query = PFQuery(className: "Places")
         query.whereKey("objectId", contains: chosenPlaceId)
         query.findObjectsInBackground { (objects, error) in
             if error != nil {
                 
             }else {
-                print(objects)
+                if objects != nil {
+                    if objects!.count > 0 {
+                        let chosenPlaceObject = objects![0]
+                        if let placeName = chosenPlaceObject.object(forKey: "name") as? String {
+                            
+                            self.detailsNameLabel.text = placeName
+                            
+                            }
+                        if let placeType = chosenPlaceObject.object(forKey: "type") as? String {
+                            
+                            self.detailsTypeLabel.text = placeType
+                            
+                            }
+                        if let placeAtmosphere = chosenPlaceObject.object(forKey: "atmosphere") as? String {
+                            
+                            self.detailsAtmosphereLabel.text = placeAtmosphere
+                            
+                        }
+                        
+                        if let placeLatitude  = chosenPlaceObject.object(forKey: "latitude") as? String {
+                            
+                            if let placeLatitudeDouble = Double(placeLatitude) {
+                                self.chosenLatitude = placeLatitudeDouble
+                            }
+                        }
+                        
+                        if let placeLongitude = chosenPlaceObject.object(forKey: "longitude") as? String {
+                            
+                            if let placeLongitudeDouble = Double(placeLongitude) {
+                                self.chosenLongitude = placeLongitudeDouble
+                            }
+                        }
+                        
+                        if let imageData = chosenPlaceObject.object(forKey: "image") as? PFFileObject {
+                            imageData.getDataInBackground { (data, error) in
+                                if error == nil {
+                                    if data != nil {
+                                        self.detailsImageView.image = UIImage(data: data!)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-        
-        
-        // Do any additional setup after loading the view.
     }
 }
+
+
